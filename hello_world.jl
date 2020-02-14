e8934b3940f3805d2d1c5e82264f62af889ac09b
Yukichi Yamaguchi <git@yamasnet.com>
2020-02-14 09:50:46 +0900

    adding hello_world.jl for testing

---
+++ hello_world.jl
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Julia 1.3.1
+#     language: julia
+#     name: julia-1.3
+# ---
+
+println(stdout,"Hello World")
+
+println(stdout,"Hello World01")
